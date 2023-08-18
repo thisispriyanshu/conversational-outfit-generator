7db6a5c1ec2df0a42f969449ff294451a66528d5
Priyanshu Agrawal <priyanshuagrawal801@gmail.com>
2023-08-19 02:19:34 +0530

    done generation

--- backend/controllers/outfitController.js
+++ backend/controllers/outfitController.js
@@ -1,8 +1,7 @@
-const { Configuration, OpenAIApi } = require("openai");
-const configuration = new Configuration({
-  apiKey: process.env.APIKEY,
+const { OpenAI } = require("openai");
+const openai = new OpenAI({
+  apiKey: process.env.APIKEY, // defaults to process.env["OPENAI_API_KEY"]
 });
-const openai = new OpenAIApi(configuration);
 
 // @desc openAI IMAGE
 // @Route POST /image
@@ -10,12 +9,12 @@ const openai = new OpenAIApi(configuration);
 const generateOutfit = async (req, res) => {
   const { prompt } = req.body;
   try {
-    const result = await openai.createImage({
+    const result = await openai.images.generate({
       prompt,
       n: 1,
       size: "256x256",
     });
-    return res.json({ result: result.data.data[0].url });
+    return res.json({ result: result.data });
   } catch (error) {
     if (error.response) {
       console.log(error.response.status);
