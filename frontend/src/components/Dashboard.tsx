e193fad01777f6b07bfa75d61f4a223240704937
Priyanshu Agrawal <priyanshuagrawal801@gmail.com>
2023-08-21 04:44:11 +0530

    project complete

--- frontend/src/components/Dashboard.tsx
+++ frontend/src/components/Dashboard.tsx
@@ -4,6 +4,7 @@ import ChatBox from './ChatBox';
 import axios from 'axios';
 
 
+
 const Dashboard: React.FC = () => {
   const [imageData, setImageData] = useState([]); // State to hold the image data
 
@@ -12,12 +13,16 @@ const Dashboard: React.FC = () => {
     setImageData(data);
   };
 
+
   return (
     <Container fluid className="mainDashboard">
       <div className="text-center mb-5 dashBoardHeading font-size">FlipFind</div>
       <Row>
         <Col sm={4}>
           <div className="scrollable-cards">
+            <div className="text-center mb-3">Similar Images</div>
+            <div className="text-center mb-3">Click on the image to view the product</div>
+            <div className="text-center mb-3">Please wait for a min after image generation to get similar products</div>
             <Card className="mb-4 text-center">
               <Card.Body>
                 {imageData.slice(0, 5).map((imageUrl, index) => (
